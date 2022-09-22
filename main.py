85c11d3d1e75a5149ed222298a84921cdf423287
Gianmauro Cuccuru <gmauro@gmail.com>
2022-09-22 17:31:03 +0200

    add more checks

--- main.py
+++ main.py
@@ -36,9 +36,43 @@ def main():
             logger.info('Checking {}'.format(fp))
             adat = canopy.read_adat(str(fp))
             shape = adat.shape[1]
-            counters = adat.index.get_level_values('SampleType')
+            sample_types = adat.index.get_level_values('SampleType')
             logger.info('{} targets found.'.format(shape))
-            logger.info('Counts of sample types: {}'.format(Counter(counters)))
+            logger.info('Counts of sample types: {}'.format(Counter(sample_types)))
+
+            # Extract flagged samples
+            flagged_samples = adat.pick_on_meta(axis=0, name='RowCheck', values=['FLAG'])
+            logger.info('{} samples are flagged (RowCheck=FLAG)'.format(len(flagged_samples)))
+            logger.info('These are the IDs:\n{}'.format(flagged_samples.index.get_level_values('SampleId')))
+
+            # Verify that external IDs are unique
+            extid_counts = Counter(adat.index.get_level_values('ExtIdentifier'))
+            extid_duplicated = [k for k, v in extid_counts.items() if v > 1]
+            logger.info('Found {} ExtIdentifier items duplicated'.format(len(extid_duplicated)))
+            logger.info('They are: {}'. format(extid_duplicated))
+
+            # Verify that sample IDs are unique
+            sample_id_counts = Counter(adat.index.get_level_values('SampleId'))
+            sample_id_duplicated = [(k, v) for k, v in sample_id_counts.items() if v > 1]
+            logger.info('Found {} SampleId items duplicated'.format(len(sample_id_duplicated)))
+            logger.info('They are: {}'.format([k for k in sample_id_duplicated]))
+
+            # Verify UniProt entries are unique
+            uniprot_entries_counts = Counter(adat.columns.get_level_values('UniProt'))
+            uniprot_entries_duplicated = [k for k, v in uniprot_entries_counts.items() if v > 1]
+            logger.info('Found {} UniProt entries duplicated'.format(len(uniprot_entries_duplicated)))
+            #logger.info('They are: {}'.format(uniprot_entries_duplicated))
+
+            # Verify Target entries are unique
+            target_entries_counts = Counter(adat.columns.get_level_values('Target'))
+            target_entries_duplicated = [k for k, v in target_entries_counts.items() if v > 1]
+            logger.info('Found {} Target entries duplicated'.format(len(target_entries_duplicated)))
+
+            # Verify SomaId entries are unique
+            somaid_entries_counts = Counter(adat.columns.get_level_values('SomaId'))
+            somaid_entries_duplicated = [k for k, v in somaid_entries_counts.items() if v > 1]
+            logger.info('Found {} SomaId entries duplicated'.format(len(somaid_entries_duplicated)))
+
     else:
         logger.error('{} path not found'.format(args.data_path))
         exit()
